bc52aa2dca42f7f8625052ae6c40465f7414fbb0
Matt Tse <tsemd@bu.edu>
2017-03-21 20:23:45 -0400

    synop

--- src/GeneralPostfixConversion.java
+++ src/GeneralPostfixConversion.java
@@ -1,5 +1,28 @@
-
+import java.io.IOException;
 
 public class GeneralPostfixConversion {
 	//infix only assuming integers no doubles
+	//TODO toPostFix
+	public String toPostFix(String[] tokens) {
+		//the formal parameter tokens in an array of tokens
+		//of an infix expression
+		
+		//the corresponding postfix expresison
+		return null;
+		
+	}
+	//TODO printStack
+	public void printStack(LinkedStack<String> st) {
+		//the formal parameter st is a linked list-based stack
+		//prints all elements of the stack beginning with the
+		//stack top element
+		//space between 2 consecutive elements
+	}
+	//TODO
+	public void main(String[] args) throws IOException {
+		//while there is an expression to read
+		//reads infix expression from input file
+		//store it in array of strings named tokens
+		//prints the converted postfix expression
+	}
 }
